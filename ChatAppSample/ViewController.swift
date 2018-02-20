e0770df02bf26d0ecd30e36df10deb12193cee3b
Masashi Tsuru <0221masa@gmail.com>
2018-02-20 23:50:21 +0900

    Initial Commit

---
+++ ChatAppSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChatAppSample
+//
+//  Created by Masashi Tsuru on 2018/02/20.
+//  Copyright © 2018年 Masashi Tsuru. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
